89bf7e4359aa5a3793ee970bbf5880bb912b9563
jun-ia <jundullah.ilhaq@gmail.com>
2022-06-14 00:20:47 +0700

    Initial Commit

---
+++ FoodMenuSwiftUI/FoodMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMenuSwiftUIApp.swift
+//  FoodMenuSwiftUI
+//
+//  Created by Jundullah Ilhaq Aulia on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
